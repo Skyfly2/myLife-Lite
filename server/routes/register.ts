a925e19bf6deba62e20f62b5deccf7dc410b130f
Asher Hamrick <ashhamrick2@gmail.com>
2020-06-26 22:33:01 -0400

    Change register to use bcrypt

--- server/routes/register.ts
+++ server/routes/register.ts
@@ -1,12 +1,14 @@
-let passwordhash = require('password-hash');
+const bcrypt = require('bcrypt');
 let con = require('./connection.ts');
 
 module.exports = async function register(user, pass, first, last, email, cb) {
 
     let query = 'INSERT INTO users (user, password, first, last, email) VALUES (?,?,?,?,?)';
-    let hashedPass = passwordhash.generate(pass);
-    con.query(query, [user, hashedPass, first, last, email], function (err, res) {
-        if (err) cb(err, 0);
-        cb(0, res);
+    bcrypt.hash(pass, 10, function (err, hash) {
+        con.query(query, [user, hash, first, last, email], function (err, res) {
+            if (err) cb(err, 0);
+            cb(0, res);
+        });
     });
+
 }
